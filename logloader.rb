a0eec0d11a472895f5d446a51648006e97147f1a
sheff3rd <thedoctorwhoovesmail@gmail.com>
2019-04-17 13:42:56 +0300

    extract logs

--- logloader.rb
+++ logloader.rb
@@ -1,3 +1,8 @@
+# require 'rubygems/package'
+# require 'zlib'
+
+require 'chilkat'
+
 BY_HOURS = Array(0..23).freeze
 
 p 'Selecte Date... (YYYY-MM-DD):'
@@ -8,8 +13,15 @@ p 'Enter papertrail token:'
 token = gets
 token.chomp!
 
+gzip = Chilkat::CkGzip.new()
+gzip.UnlockComponent('unlock')
+
 BY_HOURS.each do |hour|
   hour = format('%02d', hour)
-  system "curl --no-include -o logs-#{date}-#{hour}.tsv.gz -L -H 'X-Papertrail-Token: #{token}' \
+  system "curl --no-include -o ./tmp/logs-#{date}-#{hour}.tsv.gz -L -H 'X-Papertrail-Token: #{token}' \
           https://papertrailapp.com/api/v1/archives/#{date}-#{hour}/download"
+
+  success = gzip.UncompressFile("./tmp/logs-#{date}-#{hour}.tsv.gz", "./tmp/logs-#{date}-#{hour}.tsv")
+
+  p success ? 'Success' : 'Failure'
 end
